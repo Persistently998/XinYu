5d101400237c3935af7f6b8416c54fa322e19584
toazi <317132043@qq.com>
2020-12-07 13:24:42 +0800

    第一次测试

---
+++ src/test/java/com/xinyu/xinyu/XinyuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xinyu.xinyu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XinyuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
